ef71c087c3add2c9238a9a4a4e9318b9c9510f38
Alex <happypoo35@gmail.com>
2021-02-02 20:08:49 +0300

    first commit

---
+++ precache-manifest.4bcd1f155c6633f311ffbf5d8360dad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11d5243c21e7e71783dcabb41df1b4d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b48738b4e5eb8ae3ed9b",
+    "url": "/static/css/main.2dbf9a22.chunk.css"
+  },
+  {
+    "revision": "e3d62ff76844248a7223",
+    "url": "/static/js/2.765b4a8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.765b4a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b48738b4e5eb8ae3ed9b",
+    "url": "/static/js/main.eae50e2f.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
